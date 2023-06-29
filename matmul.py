68e3033a66d31addad89d5c5ed21b2accbe77679
Julius Sula <julius.sula@gmail.com>
2023-06-30 00:01:10 +0200

    1d blocktiling 1/2

--- matmul.py
+++ matmul.py
@@ -19,6 +19,90 @@ print(f"The local memory size of the device is {device.get_info(cl.device_info.L
 
 max_wg_size = device.get_info(cl.device_info.MAX_WORK_GROUP_SIZE)
 
+def matmul_1d_blocktiling(tensor1, tensor2):
+    a = tensor1.value.astype(np.float32)
+    b = tensor2.value.astype(np.float32)
+
+    assert tensor1.shape[1] == tensor2.shape[0]
+
+    M = np.int32(a.shape[0])
+    N = np.int32(b.shape[1])
+    K = np.int32(a.shape[1])
+
+    matmul = cl.Program(context, """
+        // has to match work_group_size
+        #define LSIZE 8
+        #define BM 32
+        #define BN 32
+        #define BK 32
+        #define TM 2
+
+        __kernel void matmul_local(__global const float* a, __global const float* b, __global float* c, const int M, const int N, const int K) {
+            const int c_row = get_group_id(1);
+            const int c_col = get_group_id(0);
+
+            // this should be a multiple of 32?
+            const int thread_row = get_local_id(1);
+            const int thread_col = get_local_id(0);
+
+            __local float a_local[BM * BK];
+            __local float b_local[BK * BN];
+
+            a += c_row * BM * K;
+            b += c_col * BN;
+            c += c_row * BM * N + c_col * BN;
+
+            // cache for each thread
+            float thread_result[TM];
+            for (int i = 0; i < TM; i++) {
+                threadResults[i] = 0.0f;
+            }
+
+            if (thread_row < M && thread_col < N) {
+                for (int block_idx=0; block_idx < K; block_idx += BK) {
+                    // load data into local memory    
+                    a_local[thread_row * BK + thread_col] = a[thread_row * K + thread_col];
+                    b_local[thread_row * BN + thread_col] = b[thread_row * N + thread_col];
+                    // synch the load
+                    barrier(CLK_LOCAL_MEM_FENCE);
+
+                    // advance pointers
+                    a += BK;
+                    b += BK * N;
+
+                    for (int i=0; i < LSIZE; i++) {
+                        sum += a_local[thread_row * LSIZE + i] * b_local[i * LSIZE + thread_col];
+                    }
+
+                    // prevent reloading cache before update of c 
+                    barrier(CLK_LOCAL_MEM_FENCE);
+                }
+                c[thread_row * N + thread_col] = sum;
+            }
+        }
+    """).build()
+
+    warp_size = matmul.matmul_local.get_work_group_info(cl.kernel_work_group_info.PREFERRED_WORK_GROUP_SIZE_MULTIPLE, device)
+    work_group_size = 8
+    global_work_size = ((M + (work_group_size - 1)) // work_group_size * work_group_size), ((N + (work_group_size - 1)) // work_group_size * work_group_size)
+    
+    a_buf = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
+    b_buf = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
+    c_np = np.zeros((a.shape[0], b.shape[1])).astype(np.float32)
+    c_buf = cl.Buffer(context, mf.WRITE_ONLY, c_np.nbytes)
+    
+    mean_elapsed = []
+    for _ in range(100):
+        kernel = matmul.matmul_local
+        kernel.set_args(a_buf, b_buf, c_buf, M, N, K)
+        event = cl.enqueue_nd_range_kernel(queue, kernel, global_work_size, (work_group_size, work_group_size))        
+        event.wait()
+        mean_elapsed.append(1e-9 * (event.profile.end - event.profile.start))
+    print(f'Local Memory = {np.mean(mean_elapsed)}')
+
+    cl.enqueue_copy(queue, c_np, c_buf)
+    return c_np
+
 def matmul_local(tensor1, tensor2):
     a = tensor1.value.astype(np.float32)
     b = tensor2.value.astype(np.float32)
